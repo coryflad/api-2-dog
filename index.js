4b2adf1b02bcf4957cc55f799a9e3caba26b8441
Cory Fladhammer <cjfladham@hotmail.com>
2019-12-14 14:48:49 -0600

    added dog display

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// passes the number value from user from to getImage
+function userInput() {
+    $("#dog-num-form").submit(e => {
+        e.preventDefault();
+        let userNum = $("#num-dog").val();
+        getImage(userNum);
+    });
+}
+
+
+$(function () {
+    console.log("App loaded! Waiting for submit!")
+    userInput();
+});
+
+// uses the number value to get the apprproiate # of images
+function getImage(numInput) {
+    if (numInput < 3) {
+        fetch("https://dog.ceo/api/breeds/image/random/3")
+            .then(response => response.json())
+            .then(responseJson => displayDogs(responseJson))
+            .catch(error => alert("Something went wrong. Try again later."));
+    } else if (numInput > 50) {
+        return alert("Please choose a number equal or less than 50");
+    } else {
+        fetch(`https://dog.ceo/api/breeds/image/random/${numInput}`)
+            .then(response => response.json())
+            .then(responseJson => displayDogs(responseJson))
+            .catch(error => alert("Something went wrong. Try again later."));
+    }
+}
+
+// adds the dog images to the DOM
+function displayDogs(responseJson) {
+    console.log(responseJson);
+    $(".results").html("");
+    responseJson.message.forEach(renderedImg => {
+      $(".results").append(`<img src="${renderedImg}" class="results">`);
+    });
+    //display the results section
+    $(".results").removeClass("hidden");
+  }
+
