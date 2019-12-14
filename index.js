2eefd65f60dd3e3504e5196f826944396537f3d5
Cory Fladhammer <cjfladham@hotmail.com>
2019-12-14 14:51:19 -0600

    moved stuff around

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 "use strict";
-
-// passes the number value from user from to getImage
-function userInput() {
-    $("#dog-num-form").submit(e => {
-        e.preventDefault();
-        let userNum = $("#num-dog").val();
-        getImage(userNum);
-    });
-}
-
-
-$(function () {
-    console.log("App loaded! Waiting for submit!")
-    userInput();
-});
-
 // uses the number value to get the apprproiate # of images
 function getImage(numInput) {
     if (numInput < 3) {
@@ -37,9 +21,26 @@ function displayDogs(responseJson) {
     console.log(responseJson);
     $(".results").html("");
     responseJson.message.forEach(renderedImg => {
-      $(".results").append(`<img src="${renderedImg}" class="results">`);
+        $(".results").append(`<img src="${renderedImg}" class="results">`);
     });
     //display the results section
     $(".results").removeClass("hidden");
-  }
+}
+
+// passes the number value from user from to getImage
+function userInput() {
+    $("#dog-num-form").submit(e => {
+        e.preventDefault();
+        let userNum = $("#num-dog").val();
+        getImage(userNum);
+    });
+}
+
+
+$(function () {
+    console.log("App loaded! Waiting for submit!")
+    userInput();
+});
+
+
 
